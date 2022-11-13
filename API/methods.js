d13e30bb72d79b0f76d44295fc196b11848efdc5
jritter77 <jr550@humboldt.edu>
2022-11-13 15:50:02 -0800

    - .env

--- API/methods.js
+++ API/methods.js
@@ -8,7 +8,7 @@ module.exports.ensureToken = function (req, res, next) {
   if (JWT) {
     jwt.verify(
       JWT,
-      '499bc4f459631403ea5165dce66678dfb971358cd0d5b644f6ca6817e7d2fe3051e56cb05fd20f086c2e25454a71d796a26907234970b951606513482bfe4083',
+      process.env.SECRET,
       (err, result) => {
         if (err) {
           res.redirect(403, "/login");
